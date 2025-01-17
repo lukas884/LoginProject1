3f7a7b8ca9f5ca75996679b469cf5265f930c7fa
lukas884 <157029390+lukas884@users.noreply.github.com>
2025-01-17 14:07:14 +0200

    got started on the styling

--- index.php
+++ index.php
@@ -16,18 +16,41 @@ $user_data = check_login($connection);
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    
+<audio autoplay loop>
+    <source src="Twin_Peaks_Theme_128kbps.mp3" type="audio/mpeg">
+    Your browser does not support this audio element.
+</audio>
+
 <div id="header">
     <a id="logout" href="logout.php">Logout</a>
         <h1>Le Website</h1>
+    <a id="logout" href="profile.php"><?php echo $user_data['user_name']; ?></a>
 </div>
 
-<div id="content"> 
-    <h1>Le index page</h1>
+<div class="navbar">
+    <a href="index.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="shop.php">Shop</a>
+    <a href="contact.php">Contact</a>
 
-    <br>
-    <h3>Welcome, <?php echo $user_data['user_name']; ?>!</h3>
+</div>
 
+<div class="welcome"> 
+    <p>Welcome, <?php echo $user_data['user_name']; ?>! Check this out...</p>
 </div>  
+
+<div class="main__content">
+
+    <div class="main__navbar">
+        <h3>Articles:</h3>
+    </div>
+
+    <div class="main__featured">
+
+    </div>
+
+</div>
     
 </body>
 </html>
